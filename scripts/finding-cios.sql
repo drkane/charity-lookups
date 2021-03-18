0d3349a6767a17b7e5ab7bf4ee913e6686b24083
David Kane <david@dkane.net>
2021-03-18 10:57:51 +0000

    add more company numbers

--- scripts/finding-cios.sql
+++
@@ -1,9 +0,0 @@
-select "CompanyNumber",
-	"CompanyName",
-	"CompanyCategory",
-	"IncorporationDate"
-from companies_main
-where "CompanyCategory" in (
-	'Charitable Incorporated Organisation', 
-	'Scottish Charitable Incorporated Organisation'
-);
